aea1970dc4decfd25c0309fb2fb4b1a3bd8e4a89
Francesco <francescofinotto@hotmail.com>
2020-07-17 23:20:07 +0200

    binded material value

--- js/gui-binder.js
+++ js/gui-binder.js
@@ -12,22 +12,43 @@ class GUIBinder{
         return this._rotation;
     }
     set rotation(value){
-        this.rotation = value;
+        this._rotation = value;
         this.updateDataInModel()
     }
+    _roughness=0.5;
+    get roughness(){
+        return this._roughness;
+    }
+    set roughness(val){
+        this._roughness = val;
+        this.updateDataInModel();
+    }
+    _metalness=0.5;
+    get metalness(){
+        return this._metalness;
+    }
+    set metalness(val){
+        this._metalness = val;
+        this.updateDataInModel();
+    }
     model;
+    mat;
     gui;
     constructor(){
     }
     setup(){
 
-        gui = new dat.GUI();
-
-        gui.add(this,'scale',0.1,3);
-        gui.add(this,'rotation',-180,180);
+        this.gui = new dat.GUI();
+        this.gui.add(this,'rotation',-Math.PI/2,Math.PI/2);
+        this.gui.add(this,'roughness',0,1);
+        this.gui.add(this,'metalness',0,1);
     }
     updateDataInModel(){
         // TODO add model transofrmation
+        // (this.model).matrixAutoUpdate = false;
+        this.model.rotation.y = this.rotation;
+        this.mat.roughness = this.roughness;
+        this.mat.metalness = this.metalness;
     }
     setModel(){
         
--- js/site.js
+++ js/site.js
@@ -1,13 +1,39 @@
+
 let canvas= new CanvasHandler("body");
 let gui = new GUIBinder();
+
 canvas.setup();
 gui.setup();
+
+let orbitControl = new THREE.OrbitControls(canvas.camera,canvas.renderer.domElement);
+let cubeMap =  new THREE.CubeTextureLoader().setPath('textures/Yokohama3/').load([
+    'posx.jpg',
+    'negx.jpg',
+    'posy.jpg',
+    'negy.jpg',
+    'posz.jpg',
+    'negz.jpg'
+]);
 let cube = new THREE.BoxGeometry(1,1,1);
-let mat = new THREE.MeshBasicMaterial({color:new THREE.Color(1,0,0)});
+let mat = new THREE.MeshPhysicalMaterial({color:new THREE.Color(1,1,1),envMap:cubeMap,metalness:1,roughness:0.5});
 let mesh = new THREE.Mesh(cube,mat);
+
+// Setup Scene
 canvas.camera.position.z = 5;
 canvas.scene.add(mesh);
+// CubeMap
+canvas.scene.background = cubeMap
+
+
+//bind model to controls
+gui.model = mesh;
+gui.mat = mat;
+console.log(gui.model);
+
+
+// Render Loop
 canvas.update=()=>{
-    (mesh).rotation.y+=0.01;
+    //mesh.rotateY(gui.rotation);
+    orbitControl.update();
 }
 canvas.renderLoop();
