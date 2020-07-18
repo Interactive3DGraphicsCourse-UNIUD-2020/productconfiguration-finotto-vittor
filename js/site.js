55e8c05ee2f5070978f4788767cd032eadb1b45c
Francesco <francescofinotto@hotmail.com>
2020-07-18 14:55:46 +0200

    added gold texture

--- js/site.js
+++ js/site.js
@@ -6,6 +6,7 @@ canvas.setup();
 gui.setup();
 
 let orbitControl = new THREE.OrbitControls(canvas.camera,canvas.renderer.domElement);
+
 let cubeMap =  new THREE.CubeTextureLoader().setPath('textures/Yokohama3/').load([
     'posx.jpg',
     'negx.jpg',
@@ -14,13 +15,17 @@ let cubeMap =  new THREE.CubeTextureLoader().setPath('textures/Yokohama3/').load
     'posz.jpg',
     'negz.jpg'
 ]);
+let light = new THREE.PointLight(new THREE.Color(1,1,1),1,5,2);
 let cube = new THREE.BoxGeometry(1,1,1);
-let mat = new THREE.MeshPhysicalMaterial({color:new THREE.Color(1,1,1),envMap:cubeMap,metalness:1,roughness:0.5});
+
+let mat = new THREE.MeshPhysicalMaterial({color:new THREE.Color(1,0,0),envMap:cubeMap,metalness:0.5,roughness:0.5});
 let mesh = new THREE.Mesh(cube,mat);
 
 // Setup Scene
+light.position.set(0,1,0);
 canvas.camera.position.z = 5;
 canvas.scene.add(mesh);
+canvas.scene.add(light);
 // CubeMap
 canvas.scene.background = cubeMap
 
